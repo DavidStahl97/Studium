f5abf60d920819e09b0af488c416037f501eea6e
DavidStahl97 <d.stahl@tus-brandoberndorf.de>
2019-06-07 16:22:41 +0200

    Grundlagen der Informatik - Formale Sprache

---
+++ GrundlagenDerInformatik/src/formaleSprache/Automat.java
@@ -0,0 +1,7 @@
+package formaleSprache;
+
+public abstract class Automat {
+	
+	public abstract boolean isInLanguage(String word);
+	
+}
---
+++ GrundlagenDerInformatik/src/formaleSprache/Main.java
@@ -0,0 +1,31 @@
+package formaleSprache;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		testKellerautomat();
+		
+	}
+	
+	private static void testDeterministicAutomat() {
+		String s = "aaaabbbabb";
+		
+		DeterministicAutomat automat = new DeterministicAutomat();
+		boolean result = automat.isInLanguage(s);
+		
+		System.out.println("Wort: " + s);
+		System.out.println("Deterministischer endlicher Automat: " + result);
+	}
+	
+	private static void testKellerautomat() {
+		String s = "(3*4+2)*9";
+		
+		Kellerautomat automat = new Kellerautomat();
+		boolean result = automat.isInLanguage(s);
+		
+		System.out.println("Wort: " + s);
+		System.out.println("Deterministischer endlicher Automat: " + result);
+	}
+	
+}
