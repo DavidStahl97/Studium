b188f3a1e57513e50f9ad5c38a16d59c7ef2d0ed
David Stahl <stahl.david1997@gmail.com>
2019-10-04 14:46:53 +0200

    Mathematik für Informatiker - Aufgabe 2.7

--- Mathematics/Mathematics/Program.cs
+++ Mathematics/Mathematics/Program.cs
@@ -7,10 +7,27 @@ namespace Mathematics
     {
         static void Main(string[] args)
         {
-            var type = typeof(IExcercise);
-            var types = AppDomain.CurrentDomain.GetAssemblies()
-                .SelectMany(s => s.GetTypes())
-                .Where(p => type.IsAssignableFrom(p) && !p.IsInterface);
+            if(args.Length < 1)
+            {
+                Console.WriteLine("Spezify which Excercise should be started.");
+                return;
+            }
+
+            var excercise = GetExcercise(args[0]);
+            excercise.Start();
+        }
+
+        private static IExcercise GetExcercise(string argument)
+        {
+            var interfaceType = typeof(IExcercise);
+            var type = AppDomain.CurrentDomain.GetAssemblies()
+                .SelectMany(a => a.GetTypes())
+                .Where(t => interfaceType.IsAssignableFrom(t) && !t.IsInterface)
+                .SingleOrDefault(t => t.Name == argument);
+
+            var instance = Activator.CreateInstance(type);
+
+            return (IExcercise)instance;
         }
     }
 }
