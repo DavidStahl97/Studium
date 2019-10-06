6c7fa361115b43af6ad7f05f4d736073ce3ebd5d
David Stahl <stahl.david1997@gmail.com>
2019-10-06 13:47:08 +0200

    Mathematik für Informatiker - Aufgabe 4.9

---
+++ Mathematics/Mathematics/Excercises/Excercise_4_9.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mathematics.Excercises
+{
+    class Excercise_4_9 : IExcercise
+    {
+        public void Start()
+        {
+            const int a = -9;
+            const int b = 3;
+
+            var mod = Modulo(a, b);
+
+            Console.WriteLine($"{a} mod {b} = {mod}");
+        }
+
+        private int Modulo(int a, int b)
+        {
+            if(a * b >= 0)
+            {
+                return a % b;
+            }
+
+            int mod = a % b;
+            if(mod == 0)
+            {
+                return mod;
+            }
+
+            return b - Math.Abs(mod);
+        }
+    }
+}
